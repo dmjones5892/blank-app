37329feaae7831369973dcf2f476726a83ff202a
dmjones5892 <125827993+dmjones5892@users.noreply.github.com>
2025-03-05 06:59:50 +0000

    Vers9Dave

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from Views import FeedView, AddPostView
+from Views.AddPostView import AddPostView
+from Views import FeedView
 from Services import get_feed, add_post
 AddPostView(add_post)
 st.write("___")
