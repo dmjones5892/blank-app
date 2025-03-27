5202b16bbcd04312b08aaec2278d49dfbf3c3718
dmjones5892 <125827993+dmjones5892@users.noreply.github.com>
2025-03-27 01:30:44 +0000

    Vers3Final

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Set page title (optional)
+st.set_page_config(page_title="Dave's Final App")
+
+# Display a header or title
+st.title("Hello from Dave, this is my Final!")
+
+# Optional: Add a brief description or any extra text
+st.write("Thank you for checking out my final Streamlit app.")
